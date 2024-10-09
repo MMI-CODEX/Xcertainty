d7ee542a7e4ac92c4217c30f9f9168cb744a2293
KC Bierlich <Kevin.bierlich@oregonstate.edu>
2024-10-09 08:53:49 -0700

    Update precompile.R

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,5 +1,3 @@
 # Pre-compiled vignettes 
 # Must manually move image files from Xcertainty/ to Xcertainty/vignettes/ after knit
 knitr::knit(input = "vignettes/Xcertainty_growth_curves.Rmd.orig", output = "vignettes/Xcertainty_growth_curves.Rmd")
-
-
