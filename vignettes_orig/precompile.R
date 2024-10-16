959dc012bb2f92af430c74e1697a95a5cf42003f
KC Bierlich <Kevin.bierlich@oregonstate.edu>
2024-10-16 11:57:41 -0700

    link updated

--- vignettes_orig/precompile.R
+++ vignettes_orig/precompile.R
@@ -4,4 +4,4 @@ knitr::knit(input = "vignettes_orig/Xcertainty.Rmd", output = "vignettes/Xcertai
 knitr::knit(input = "vignettes_orig/Xcertainty_informative_priors.Rmd", output = "vignettes/Xcertainty_informative_priors.Rmd")
 knitr::knit(input = "vignettes_orig/Xcertainty_growth_curves.Rmd", output = "vignettes/Xcertainty_growth_curves.Rmd")
 
-# Must manually move image files from Xcertainty/ to Xcertainty/vignettes/ after knit
+# Make sure image files end up in Xcertainty/vignettes/img after knit. May need to manually drag and drop.
