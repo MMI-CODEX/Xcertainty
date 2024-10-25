dc574f29e072349139da09114aaa7d581dbd004f
KC Bierlich <Kevin.bierlich@oregonstate.edu>
2024-10-25 15:57:10 -0700

    update figures to now display properly in vignettes

--- vignettes_orig/precompile.R
+++ vignettes_orig/precompile.R
@@ -4,4 +4,17 @@ knitr::knit(input = "vignettes_orig/Xcertainty.Rmd", output = "vignettes/Xcertai
 knitr::knit(input = "vignettes_orig/Xcertainty_informative_priors.Rmd", output = "vignettes/Xcertainty_informative_priors.Rmd")
 knitr::knit(input = "vignettes_orig/Xcertainty_growth_curves.Rmd", output = "vignettes/Xcertainty_growth_curves.Rmd")
 
-# Make sure image files end up in Xcertainty/vignettes/img after knit. May need to manually drag and drop.
+# {!} Make sure image files end up in Xcertainty/vignettes/img after knit. 
+# May need to manually drag and drop from 'Xcertainty/figure/' to 'Xcertainty/vignette/img/'
+
+# {!} Then, need to manually change the pathway for each vignette in the 'vignettes/' folder, by changing "figure" to "img".
+# for example, change: 
+# "![plot of chunk Xc_Fig8_bai_dist](figure/Xc_Fig8_bai_dist-1.png)" 
+# to 
+# "![plot of chunk Xc_Fig8_bai_dist](img/Xc_Fig8_bai_dist-1.png)"
+
+
+# To create local copies of html, ex:
+rmarkdown::render(input = "vignettes/Xcertainty.Rmd", output_format = "html_document")
+rmarkdown::render(input = "vignettes/Xcertainty_informative_priors.Rmd", output_format = "html_document")
+rmarkdown::render(input = "vignettes/Xcertainty_growth_curves.Rmd", output_format = "html_document")
